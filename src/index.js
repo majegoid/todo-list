f959cd151a6a94f9f5383cfe5d25d29db5f6e3df
majegoid <majegoid@gmail.com>
2023-01-02 14:01:26 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+class Todo {
+  #title;
+  #description;
+  #isComplete;
+  #dueDate;
+
+  constructor(
+    title = '',
+    description = '',
+    isComplete = false,
+    dueDate = new Date(Date.now() + 86400000)
+  ) {
+    this.#title = title;
+    this.#description = description;
+    this.#isComplete = isComplete;
+    this.#dueDate = dueDate;
+  }
+
+  get title() {
+    return this.#title;
+  }
+
+  set title(value) {
+    if (typeof value === 'string') {
+      this.#title = value;
+    }
+  }
+
+  get description() {
+    return this.#description;
+  }
+
+  set description(value) {
+    if (typeof value === 'string') {
+      this.#description = value;
+    }
+  }
+
+  get isComplete() {
+    return this.#isComplete;
+  }
+
+  set isComplete(value) {
+    if (typeof value === 'string') {
+      this.#isComplete = value;
+    }
+  }
+
+  get dueDate() {
+    return this.#dueDate;
+  }
+
+  set dueDate(value) {
+    if (typeof value === 'object') {
+      this.#dueDate = value;
+    }
+  }
+}
+
+let displayController = (function () {})();
+
+let todo = new Todo('some title', 'some description', true, new Date('2023-01-07T21:00:00'));
+
+console.log(todo);
