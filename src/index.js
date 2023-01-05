2f8d24834d1880d4851e6335eb836e7f2b4fa54f
majegoid <majegoid@gmail.com>
2023-01-05 16:44:44 -0600

    adds all components as plain html; updates css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { Todo } from './modules/todo';
 import './styles/index.css';
 
+const projectArr = [];
+
 let todo = new Todo(
   'some title',
   'some description',
   true,
   new Date('2023-01-07T21:00:00')
 );
+
+projectArr.push(todo);
 
 console.log(todo);
