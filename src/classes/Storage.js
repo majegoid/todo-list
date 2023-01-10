9c582b3018a02e3a9efc245fa348c5523075a05a
majegoid <majegoid@gmail.com>
2023-01-09 18:10:30 -0600

    updates dummy data to help differentiate between projects

--- src/classes/Storage.js
+++ src/classes/Storage.js
@@ -20,27 +20,65 @@ export class Storage {
   static seedData() {
     const todo1 = new Todo(
       'Todo #1',
-      'Description',
+      'Description 1',
       true,
       new Date('2023-01-07T21:00:00')
     );
     const todo2 = new Todo(
       'Todo #2',
-      'Description',
+      'Description 2',
       true,
       new Date('2023-01-07T21:00:00')
     );
     const todo3 = new Todo(
       'Todo #3',
-      'Description',
+      'Description 3',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+
+    const todo4 = new Todo(
+      'Todo #4',
+      'Description 4',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+    const todo5 = new Todo(
+      'Todo #5',
+      'Description 5',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+    const todo6 = new Todo(
+      'Todo #6',
+      'Description 6',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+
+    const todo7 = new Todo(
+      'Todo #7',
+      'Description 7',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+    const todo8 = new Todo(
+      'Todo #8',
+      'Description 8',
+      true,
+      new Date('2023-01-07T21:00:00')
+    );
+    const todo9 = new Todo(
+      'Todo #9',
+      'Description 9',
       true,
       new Date('2023-01-07T21:00:00')
     );
 
     const projectList = [
       new Project('Project #1', [todo1, todo2, todo3]),
-      new Project('Project #2', [todo1, todo2, todo3]),
-      new Project('Project #3', [todo1, todo2, todo3]),
+      new Project('Project #2', [todo4, todo5, todo6]),
+      new Project('Project #3', [todo7, todo8, todo9]),
     ];
 
     for (const project of projectList) {
