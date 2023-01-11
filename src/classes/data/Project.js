da380a8d9d8e66ea25b5ae136e94e01671b711b9
majegoid <majegoid@gmail.com>
2023-01-11 12:23:39 -0600

    adds documentation comments to Project.js

--- src/classes/data/Project.js
+++ src/classes/data/Project.js
@@ -1,6 +1,7 @@
 import { Persistence } from '../static/Persistence';
 import { Todo } from './Todo';
 
+/** A Project has a title and a todoList. */
 export class Project {
   #title;
   #todoList;
@@ -10,20 +11,25 @@ export class Project {
     this.#todoList = todoList;
   }
 
+  /** Gets the title of the Project. */
   get title() {
     return this.#title;
   }
 
+  /** Sets the title of the Project. */
   set title(value) {
     if (typeof value === 'string') {
       this.#title = value;
     }
   }
 
+  /** Gets a copy of the todoList of the Project. */
   get todoList() {
     return [...this.#todoList];
   }
 
+  /** Sets the todoList of the project. Does not set if the input is not an Array with only Todo
+   * elements. */
   set todoList(value) {
     if (!(value instanceof Array)) {
       return;
@@ -36,6 +42,7 @@ export class Project {
     this.#todoList = value;
   }
 
+  /** Adds one Todo to the Project's todoList. Overwrites the Project in localStorage. */
   addTodo(todo) {
     if (!(todo instanceof Todo)) {
       return;
@@ -44,6 +51,7 @@ export class Project {
     Persistence.setProject(this);
   }
 
+  /** Removes one Todo from the Project's todoList. Overwrites the Project in localStorage. */
   removeTodo(todo) {
     if (!(todo instanceof Todo)) {
       return;
@@ -52,6 +60,7 @@ export class Project {
     Persistence.setProject(this);
   }
 
+  /** Returns an JSON serializable object that represents this Project. */
   toJSON = function () {
     return { title: this.title, todoList: [...this.todoList] };
   };
